ef875ded3728da5ad1ead1407a953ffebafc90e0
mnardi <mnardi@daitan.com>
2019-10-04 08:56:39 -0300

    Second test with Guice Injector

---
+++ my-guice/src/main/java/BillingService.java
@@ -0,0 +1,61 @@
+package com.mnardi.javalearning.myguice;
+
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Inject;
+import com.google.inject.Guice;
+import java.lang.*;
+
+public class BillingService  {
+    private final CreditCardProcessor processor;
+    private final TransactionLog transactionLog;
+    private int counter;
+
+
+    @Inject
+    BillingService(  CreditCardProcessor processor,
+                     TransactionLog transactionlog)
+    {
+        this.processor = processor;
+        this.transactionLog = transactionlog;
+        this.counter = counter;
+    }
+
+
+
+    public boolean chargeOrder(long charge)
+    {
+        counter++;
+        return processor.process(charge);
+    }
+
+    public void logTotalCharge()
+    {
+        System.out.println("Total charged in " + counter + " operations:  " +  this.processor.getCharged());
+    }
+
+    public static void main(String[] args)
+    {
+
+        //Injector injector = Guice.createInjector(new BillingModule());
+        Injector injector = Guice.createInjector(new AbstractModule()
+                                                {
+                                                   protected void configure() {
+                                                        bind(TransactionLog.class).to(TransactionLogDummy.class);
+                                                        bind(CreditCardProcessor.class).to(CreditCardDoubleDummy.class);
+                                                    }
+                                              });
+
+
+        BillingService billingService = injector.getInstance(BillingService.class);
+        billingService.chargeOrder(10);
+        billingService.chargeOrder(8);
+        billingService.chargeOrder(-5);
+        billingService.logTotalCharge();
+
+
+
+    }
+
+}
---
+++ my-guice/src/main/java/CreditCardDummy.java
@@ -0,0 +1,24 @@
+package com.mnardi.javalearning.myguice;
+
+public class CreditCardDummy implements CreditCardProcessor{
+    CreditCardDummy()
+    {
+        this.charge=0;
+    }
+
+    @Override
+    public boolean process(long charge)
+    {
+        this.charge += charge;
+        return true;
+    }
+
+    @Override
+    public long getCharged()
+    {
+        return this.charge;
+    }
+
+    private long charge;
+
+}
